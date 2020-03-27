250a0887144126f5995759f40144ea5bf184a4d0
David Iglesias <david.iglesiasechevarria@colorado.edu>
2020-03-27 10:34:12 -0700

    add ransac for planes

--- src/quiz/ransac/ransac2d.cpp
+++ src/quiz/ransac/ransac2d.cpp
@@ -61,7 +61,75 @@ pcl::visualization::PCLVisualizer::Ptr initScene()
   	return viewer;
 }
 
-std::unordered_set<int> Ransac(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, int maxIterations, float distanceTol)
+std::unordered_set<int> RansacPlane(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, int maxIterations, float distanceTol)
+{
+	std::unordered_set<int> inliersResult;
+	srand(time(NULL));
+
+	int best_num_inliers = 0;
+	std::unordered_set<int> inliers;
+
+	for (int i = 0; i < maxIterations; i++)
+	{
+		int num_inliers = 0;
+
+		// Randomly sample subset and fit line
+		int idx1 = rand() % cloud->points.size();
+		int idx2 = idx1;
+		int idx3 = idx1;
+		while (idx2 == idx1)
+		{
+			idx2 = rand() % cloud->points.size();
+		}
+		while (idx3 == idx1 || idx3 == idx2)
+		{
+			idx3 = rand() % cloud->points.size();
+		}
+
+		// Define required vectors
+		Eigen::Vector3f v1(cloud->points[idx2].x - cloud->points[idx1].x,
+						   cloud->points[idx2].y - cloud->points[idx1].y,
+						   cloud->points[idx2].z - cloud->points[idx1].z);
+		Eigen::Vector3f v2(cloud->points[idx3].x - cloud->points[idx1].x,
+						   cloud->points[idx3].y - cloud->points[idx1].y,
+						   cloud->points[idx3].z - cloud->points[idx1].z);
+		Eigen::Vector3f v1xv2 = v1.cross(v2);
+
+		// 4 values define a plane in 3D: Ax + By + Cz + D = 0
+		float A, B, C, D;
+		A = v1xv2(0);
+		B = v1xv2(1);
+		C = v1xv2(2);
+		D = -(v1xv2(0) * v1(0) + v1xv2(1) * v1(1) + v1xv2(2) * v1(2));
+
+		// Distance
+		for (int j = 0; j < cloud->points.size(); j++)
+		{
+			auto& p = cloud->points[j];
+			float d = fabs(A * p.x + B * p.y + C * p.z + D) / sqrt(A * A + B * B + C * C);
+			// std::cout << "A, B, C" << A << " " << B << " " << C << " distance: " << d << std::endl;
+			if (d < distanceTol)
+			{
+				inliers.insert(j);
+				num_inliers++;
+			}
+		}
+		std::cout << "iter: " << i << ", inliers: " << num_inliers << std::endl;
+
+		if (num_inliers > best_num_inliers)
+		{
+			inliersResult.clear();
+			inliersResult = inliers;
+			best_num_inliers = num_inliers;
+		}
+		inliers.clear();
+	}
+	
+	return inliersResult;
+
+}
+
+std::unordered_set<int> RansacLine(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, int maxIterations, float distanceTol)
 {
 	std::unordered_set<int> inliersResult;
 	srand(time(NULL));
@@ -124,7 +192,7 @@ int main ()
 	
 
 	// TODO: Change the max iteration and distance tolerance arguments for Ransac function
-	std::unordered_set<int> inliers = Ransac(cloud, 50, 0.5);
+	std::unordered_set<int> inliers = RansacPlane(cloud, 50, 0.5);
 
 	pcl::PointCloud<pcl::PointXYZ>::Ptr  cloudInliers(new pcl::PointCloud<pcl::PointXYZ>());
 	pcl::PointCloud<pcl::PointXYZ>::Ptr cloudOutliers(new pcl::PointCloud<pcl::PointXYZ>());
